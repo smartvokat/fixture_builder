dcd44931bfba5f0a3f5274b96ca1baaf457e0468
Christian Schuhmann <97274+madebyherzblut@users.noreply.github.com>
2021-01-12 20:56:44 +0100

    test: Fix equality check

--- test/fixture_builder_test.exs
+++ test/fixture_builder_test.exs
@@ -7,7 +7,7 @@ defmodule FixtureBuilderTest do
 
   describe "setup()" do
     test "returns a simple result" do
-      assert %{key: :some_atom} = Fixtures.fixtures(put(:key, :atom))
+      assert %{key: :some_atom} == Fixtures.fixtures(put(:key, :atom))
     end
   end
 
@@ -20,11 +20,11 @@ defmodule FixtureBuilderTest do
 
   describe "put()" do
     test "puts the result into a simple path" do
-      assert %{key: :some_atom} = Fixtures.fixtures(put(:key, :atom))
+      assert %{key: :some_atom} == Fixtures.fixtures(put(:key, :atom))
     end
 
     test "puts the result into a nested path" do
-      assert %{some: ["hello world"]} =
+      assert %{some: ["hello world"]} ==
                Fixtures.fixtures([
                  put(:some, :custom_value, %{value: [[]]}),
                  put([:some, 0], :custom_value, %{value: "hello world"})
@@ -32,7 +32,7 @@ defmodule FixtureBuilderTest do
     end
 
     test "sets the parent correctly" do
-      assert %{a_key: %{hello: "world", key: %{asserted_map: true}}} =
+      assert %{a_key: %{hello: "world", key: %{asserted_map: true}}} ==
                Fixtures.fixtures([
                  put(:a_key, :custom_value, %{value: %{hello: "world"}}, [
                    put(:key, :assert_parent_is_map)
@@ -41,7 +41,7 @@ defmodule FixtureBuilderTest do
     end
 
     test "supports nested operations" do
-      assert %{some: ["hello world"]} =
+      assert %{some: ["hello world"]} ==
                Fixtures.fixtures([
                  put(:some, :custom_value, %{value: [[]]}, [
                    put(0, :custom_value, %{value: "hello world"})
@@ -50,7 +50,7 @@ defmodule FixtureBuilderTest do
     end
 
     test "supports functions as args" do
-      assert %{a: %{hello: "world"}, b: "world"} =
+      assert %{a: %{hello: "world"}, b: "world"} ==
                Fixtures.fixtures([
                  put(:a, :custom_value, %{value: %{hello: "world"}}),
                  put(:b, :custom_value, &%{value: &1.a.hello})
@@ -58,19 +58,19 @@ defmodule FixtureBuilderTest do
     end
 
     test "supports composition" do
-      assert %{comp: %{a: :map_comp, b: :map_comp}} = Fixtures.fixtures(put(:comp, :map_comp))
+      assert %{comp: %{a: :map_comp, b: :map_comp}} == Fixtures.fixtures(put(:comp, :map_comp))
 
-      assert %{comp: %{a: "hello", b: "hello"}} =
+      assert %{comp: %{a: "hello", b: "hello"}} ==
                Fixtures.fixtures([put(:comp, :map_comp, %{value: "hello"})])
 
-      assert %{comp: [:list_comp, :list_comp]} = Fixtures.fixtures(put(:comp, :list_comp))
+      assert %{comp: [:list_comp, :list_comp]} == Fixtures.fixtures(put(:comp, :list_comp))
 
-      assert %{comp: ["world", "world"]} =
+      assert %{comp: ["world", "world"]} ==
                Fixtures.fixtures(put(:comp, :list_comp, %{value: "world"}))
     end
 
     test "supports composition and nested operations" do
-      assert %{comp: %{a: :map_comp, b: :map_comp, hello: "world"}} =
+      assert %{comp: %{a: :map_comp, b: :map_comp, hello: "world"}} ==
                Fixtures.fixtures([
                  put(:comp, :map_comp, [
                    put(:hello, :custom_value, %{value: "world"})
@@ -81,9 +81,9 @@ defmodule FixtureBuilderTest do
 
   describe "merge()" do
     test "merges the result correctly" do
-      assert %{a: "hello", b: "world"} = Fixtures.fixtures([merge(:simple_map)])
+      assert %{a: "hello", b: "world"} == Fixtures.fixtures([merge(:simple_map)])
 
-      assert %{a: "hello", b: "world", c: %{a: "hello", b: "world"}} =
+      assert %{a: "hello", b: "world", c: %{a: "hello", b: "world"}} ==
                Fixtures.fixtures([
                  merge(:simple_map),
                  put(:c, :custom_value, %{value: %{}}, [
@@ -93,7 +93,7 @@ defmodule FixtureBuilderTest do
     end
 
     test "sets the parent correctly" do
-      assert %{a_key: %{asserted_map: true}} =
+      assert %{a_key: %{asserted_map: true, hello: "world"}} ==
                Fixtures.fixtures([
                  put(:a_key, :custom_value, %{value: %{hello: "world"}}, [
                    merge(:assert_parent_is_map)
@@ -102,7 +102,7 @@ defmodule FixtureBuilderTest do
     end
 
     test "supports functions as args" do
-      assert %{a: "hello", b: "world", c: "hello bob"} =
+      assert %{a: "hello", b: "world", c: "hello bob"} ==
                Fixtures.fixtures([
                  merge(:simple_map),
                  merge(:custom_value, &%{value: %{c: "#{&1.a} bob"}})
@@ -123,11 +123,11 @@ defmodule FixtureBuilderTest do
     end
 
     test "puts the result correctly" do
-      assert %{hello: :world} = Fixtures.fixtures([run(:hello, fn _, _ -> :world end)])
+      assert %{hello: :world} == Fixtures.fixtures([run(:hello, fn _, _ -> :world end)])
     end
 
     test "handle multiple runs correctly" do
-      assert %{hello: :bob} =
+      assert %{hello: :bob} ==
                Fixtures.fixtures([
                  run(:hello, fn _, _ -> :world end),
                  run(:hello, fn _, _ -> :bob end)
@@ -139,7 +139,7 @@ defmodule FixtureBuilderTest do
     test "appends the result correctly" do
       assert [:simple_atom] = Fixtures.new([], %{}, [append(:simple_atom)]) |> Fixtures.fixtures()
 
-      assert %{list: [:simple_atom]} =
+      assert %{list: [:simple_atom]} ==
                Fixtures.new(%{list: []}, %{}, [append([:list], :simple_atom)])
                |> Fixtures.fixtures()
     end
